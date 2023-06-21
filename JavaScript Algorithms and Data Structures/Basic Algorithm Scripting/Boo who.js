75a24321dc61fbd7310ed75fb4fb2b771807642e
elspirito <el_spirito@bk.ru>
2023-06-21 20:00:35 +0300

    Boo who

---
+++ JavaScript Algorithms and Data Structures/Basic Algorithm Scripting/Boo who.js
@@ -0,0 +1,10 @@
+function booWho(bool) {
+    if (typeof bool === "boolean") {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+console.log(booWho(null));
+console.log(booWho(true));
